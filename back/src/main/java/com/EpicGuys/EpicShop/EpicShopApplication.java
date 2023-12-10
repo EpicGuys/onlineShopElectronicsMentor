ea3fd311f508ff43eb3393e38f49875335d22834
Stepashka369 <kardash-99@list.ru>
2023-12-10 12:44:51 +0300

    Back: project have been created

---
+++ back/src/main/java/com/EpicGuys/EpicShop/EpicShopApplication.java
@@ -0,0 +1,13 @@
+package com.EpicGuys.EpicShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicShopApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/EpicGuys/EpicShop/EpicShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EpicGuys.EpicShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
